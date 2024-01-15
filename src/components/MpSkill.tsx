d186c53e00f8c2fb6c32e4536d48f7b88fd5e34b
mirko.petrelli <mirko.petrelli@avanade.com>
2024-01-15 16:52:40 +0100

    Added 3 skills

--- src/components/MpSkill.tsx
+++ src/components/MpSkill.tsx
@@ -8,47 +8,54 @@ interface MpSkillProps {
 
 const MAX = 630;
 
-export function MpSkill( { name, value, imageUrl }: MpSkillProps) {
+export function MpSkill({ name, value, imageUrl }: MpSkillProps) {
+  const x = (MAX * value) / 100;
 
-  const x = MAX * value / 100;
-
-  return <div className="flex flex-col items-center animate-on-view">
-
-  <div title={`${name} - ${value}%`}>
-    <svg width="100%" height="100%" viewBox='0 0 300 300' >
-      <circle 
-        cx="150" cy="150" r="100" 
-        fill="transparent"
-        stroke="white"
-        strokeWidth={10}
-        strokeDasharray={"3 8"}
-      />
-      <circle  
-        cx="150" cy="150" r="100" 
-        className="animate-strokeSpin"
-        stroke='white'
-        strokeWidth={10} 
-        strokeDasharray={`${x} ${MAX - x}`}
-        strokeDashoffset={158}
-        fill="transparent"
-      />
-
-      <foreignObject x="100" y="100" width="100" height="100" className="pointer-events-none">
-        <div>
-          <Image
-            src={imageUrl}
-            alt="TODO:"
-            
-            width={100}
-            height={100}
+  return (
+    <div className="flex flex-col items-center">
+      <div title={`${name} - ${value}%`}>
+        <svg width="100%" height="100%" viewBox="0 0 300 300">
+          <circle
+            cx="150"
+            cy="150"
+            r="100"
+            fill="transparent"
+            stroke="white"
+            strokeWidth={10}
+            strokeDasharray={"3 8"}
           />
-        </div>
-      </foreignObject>
-    </svg>
+          <circle
+            cx="150"
+            cy="150"
+            r="100"
+            className="animate-strokeSpin"
+            stroke="white"
+            strokeWidth={10}
+            strokeDasharray={`${x} ${MAX - x}`}
+            strokeDashoffset={158}
+            fill="transparent"
+          />
 
-    <h5 className="relative -top-3">{name}</h5>
-    
-  </div>
-  </div>
+          <foreignObject
+            x="100"
+            y="100"
+            width="100"
+            height="100"
+            className="pointer-events-none"
+          >
+            <div>
+              <Image 
+                src={imageUrl} 
+                alt={`${name} logo`} 
+                width={100} 
+                height={100} 
+              />
+            </div>
+          </foreignObject>
+        </svg>
 
+        <h5 className="relative -top-3">{name}</h5>
+      </div>
+    </div>
+  );
 }
